084cc1ca0a0e10a987697f4334b91ea8f0843ccd
sidthakur08 <41535376+sidthakur08@users.noreply.github.com>
2021-03-01 02:42:52 -0800

    finished big_hw

--- flights_delays/prac_work.R
+++ flights_delays/prac_work.R
@@ -27,6 +27,8 @@ ggplot(time_departure_delay,aes(y=mean_dep_delay,x=hour)) +
 colnames(weather)
 colSums(is.na(weather))
 
+unique(weather$visib)
+
 temp_departure_delay <- flights %>%
   left_join(weather,by=c("origin","time_hour")) %>%
   group_by(temp) %>%
@@ -35,19 +37,26 @@ temp_departure_delay <- flights %>%
 
 ggplot(temp_departure_delay, aes(y=mean_dep_delay,x=temp)) +
   geom_line() +
-  geom_smooth(method = 'loess') + 
+  geom_smooth(method = 'loess',se = FALSE) + 
   labs(x = "Temperature",
        y = "Departure Delay of Flight",
        title = "Mean Departure Delay of Flights vs Temperature at the Airport",
        caption = "Data Source: nycflights13")
 
-
-# 
+# To check which airlines handles departure delay well and at what airport
 airline_departure_delay <- flights %>% 
   left_join(airlines,by="carrier") %>%
   group_by(origin,name) %>%
   summarise(mean_dep_delay = mean(dep_delay,na.rm = TRUE))
 
-ggplot(airline_departure_delay) +
-  geom_point(mapping = aes(x = name, y=mean_dep_delay, color=origin)) +
+ggplot(airline_departure_delay, mapping = aes(x = name, y=mean_dep_delay, color=origin)) +
+  geom_point() + 
+  geom_line() +
   theme(axis.text.x = element_text(angle = 90))
+
+
+airlines_only <- airline_departure_delay %>%
+  group_by(name) %>%
+  summarise(mean_dep_delay= mean(mean_dep_delay)) %>%
+  arrange(mean_dep_delay)
+airlines_only
